224721b786693747e69931ee9911ed9f8d6edc3f
jefferson lennon <jeffersonlennon1@outlook.com>
2020-07-17 22:06:32 -0300

    Escopo da filtragem. Estou usando a criteria do jpa

--- src/main/java/com/example/mercadinho/repository/produto/ProdutoRepositoryImpl.java
+++ src/main/java/com/example/mercadinho/repository/produto/ProdutoRepositoryImpl.java
@@ -1,16 +1,44 @@
 package com.example.mercadinho.repository.produto;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import com.example.mercadinho.model.Produto;
 import com.example.mercadinho.repository.filter.ProdutoFilter;
 
 public class ProdutoRepositoryImpl implements ProdutoRepositoryQuery{
 
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@Override
 	public List<Produto> filtrar(ProdutoFilter produtoFilter) {
-		// TODO Auto-generated method stub
-		return null;
+		CriteriaBuilder builder = manager.getCriteriaBuilder();
+		CriteriaQuery<Produto> criteria = builder.createQuery(Produto.class);
+		Root<Produto> root = criteria.from(Produto.class);
+
+		
+		Predicate[] predicates = criarRestricoes(produtoFilter, builder, root);
+		criteria.where(predicates);
+		
+		
+		TypedQuery<Produto> query = manager.createQuery(criteria);
+		return query.getResultList();
+		}
+
+	private Predicate[] criarRestricoes(ProdutoFilter produtoFilter, CriteriaBuilder builder, Root<Produto> root) {
+		List<Predicate> predicates = new ArrayList<>();
+		
+		
+		return predicates.toArray(new Predicate[predicates.size()]);
 	}
 
 }
