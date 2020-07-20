96f25a6c86ebffc2e4c97fcbbbef9b3f0bea5190
jefferson lennon <jeffersonlennon1@outlook.com>
2020-07-20 18:38:08 -0300

    Comecando a implementar a segurança da api

--- src/main/java/com/example/mercadinho/service/ProdutoService.java
+++ src/main/java/com/example/mercadinho/service/ProdutoService.java
@@ -1,16 +1,9 @@
 package com.example.mercadinho.service;
 
-import java.net.URI;
-
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
-import org.springframework.web.client.HttpServerErrorException;
 
-import com.example.mercadinho.event.MercadinhoRecursoCriadoEvent;
 import com.example.mercadinho.model.Produto;
 import com.example.mercadinho.repository.ProdutoRepository;
 import com.example.mercadinho.service.exception.RecursoNaoExisteException;
