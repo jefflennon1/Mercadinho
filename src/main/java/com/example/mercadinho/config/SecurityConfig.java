d8bab4fdaa9be08c0b725c0aef57db8f70b99680
jefferson lennon <jeffersonlennon1@outlook.com>
2020-07-20 18:50:03 -0300

    implementando basica auth - inicio da segurança

--- src/main/java/com/example/mercadinho/config/SecurityConfig.java
+++ src/main/java/com/example/mercadinho/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.example.mercadinho.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 
 @Configuration
@@ -13,6 +15,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication().withUser("root").password("root").roles("ROLE");
-		
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.anyRequest().authenticated()
+		.and()
+		.httpBasic()
+		.and()
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and()
+		.csrf().disable();
 	}
 }
