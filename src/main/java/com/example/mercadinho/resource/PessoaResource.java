e006be408a22d2e05aedc136c4f624e953187d70
jefferson lennon <jeffersonlennon1@outlook.com>
2020-07-25 09:47:17 -0300

    Implementando a busca de pessoa

--- src/main/java/com/example/mercadinho/resource/PessoaResource.java
+++ src/main/java/com/example/mercadinho/resource/PessoaResource.java
@@ -3,13 +3,16 @@ package com.example.mercadinho.resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.mercadinho.model.Pessoa;
 import com.example.mercadinho.repository.PessoaRepository;
 import com.example.mercadinho.repository.filter.PessoaFilter;
+import com.example.mercadinho.service.PessoaService;
 
 @RestController
 @RequestMapping("/pessoas")
@@ -18,11 +21,17 @@ public class PessoaResource {
 	@Autowired
 	private PessoaRepository pessoaRepository;
 	
-	
+	@Autowired
+	private PessoaService pessoaService;
 	
 	@GetMapping
-	public Page<Pessoa> listar(PessoaFilter pessoaFilter, Pageable pageable){
-			
+	public Page<Pessoa> listar(PessoaFilter pessoaFilter, Pageable pageable){		
 		return pessoaRepository.filtrar(pessoaFilter, pageable);
+	}
+	
+	@GetMapping("{id}")
+	public ResponseEntity<Pessoa> buscarPessoa(@PathVariable Long id){
+		Pessoa pessoa = pessoaService.buscarPeloId(id);
+		return ResponseEntity.ok(pessoa);
 	}
 }
---
+++ src/main/java/com/example/mercadinho/service/PessoaService.java
@@ -0,0 +1,25 @@
+package com.example.mercadinho.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import com.example.mercadinho.model.Pessoa;
+import com.example.mercadinho.repository.PessoaRepository;
+import com.example.mercadinho.service.exception.RecursoNaoExisteException;
+
+@Service
+public class PessoaService {
+
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	public Pessoa buscarPeloId(Long id) {
+		Pessoa pessoa = pessoaRepository.findOne(id);
+		if(StringUtils.isEmpty(pessoa)) {
+			throw new RecursoNaoExisteException();
+		}
+		return pessoa;
+	}
+
+}
