812631c993769e64fac30cb72922cf88bccecbbf
jefferson lennon <jeffersonlennon1@outlook.com>
2020-08-01 15:00:04 -0300

    Iniciando o método que complementa o controle de autenticação

--- src/main/java/com/example/mercadinho/config/AuthorizationServerConfig.java
+++ src/main/java/com/example/mercadinho/config/AuthorizationServerConfig.java
@@ -1,11 +1,15 @@
 package com.example.mercadinho.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -22,5 +26,16 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 		.scopes("read","write")
 		.authorizedGrantTypes("password")
 		.accessTokenValiditySeconds(3600);
+	}
+	
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.tokenStore(tokenStore())
+	}
+
+	@Bean
+	private TokenStore tokenStore() {
+		
+		return new InMemoryTokenStore();
 	}
 }
