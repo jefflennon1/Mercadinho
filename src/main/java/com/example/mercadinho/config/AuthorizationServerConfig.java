80b171caefc7ccf564378860cbd81e7d81850b75
jefferson lennon <jeffersonlennon1@outlook.com>
2020-08-01 14:56:23 -0300

    começando a implemtar o método de autenticação do meu cliente e definindo o tempo que o token vai permanecer válido

--- src/main/java/com/example/mercadinho/config/AuthorizationServerConfig.java
+++ src/main/java/com/example/mercadinho/config/AuthorizationServerConfig.java
@@ -1,7 +1,26 @@
 package com.example.mercadinho.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
+@Configuration
+@EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
+	@Autowired
+	private AuthenticationManager manager;
+	
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.inMemory()
+		.withClient("@angular")
+		.secret("angular@angular")
+		.scopes("read","write")
+		.authorizedGrantTypes("password")
+		.accessTokenValiditySeconds(3600);
+	}
 }
