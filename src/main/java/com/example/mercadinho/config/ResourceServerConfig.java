3556f728063c75b1ac14ee6659f4235b44921273
jefferson lennon <jeffersonlennon1@outlook.com>
2020-07-31 21:31:48 -0300

    consertando a inseção de dependencias da minha classe

--- src/main/java/com/example/mercadinho/config/ResourceServerConfig.java
+++ src/main/java/com/example/mercadinho/config/ResourceServerConfig.java
@@ -1,24 +1,25 @@
 package com.example.mercadinho.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 
 @Configuration
 @EnableWebSecurity
-public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
-	@Override
+	@Autowired
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication().withUser("root").password("root").roles("ROLE");
 	}
 	
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 		.anyRequest().authenticated()
 		.and()
