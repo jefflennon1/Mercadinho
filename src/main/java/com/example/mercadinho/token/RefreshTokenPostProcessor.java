3dab30c9eba976c1a9caff729d072b99416f79f7
jefferson lennon <jeffersonlennon1@outlook.com>
2020-08-11 19:50:17 -0300

    Criando o cookie

--- src/main/java/com/example/mercadinho/token/RefreshTokenPostProcessor.java
+++ src/main/java/com/example/mercadinho/token/RefreshTokenPostProcessor.java
@@ -1,10 +1,16 @@
 package com.example.mercadinho.token;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.http.server.ServletServerHttpResponse;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
@@ -21,8 +27,21 @@ public class RefreshTokenPostProcessor implements ResponseBodyAdvice<OAuth2Acces
 	public OAuth2AccessToken beforeBodyWrite(OAuth2AccessToken body, MethodParameter returnType,
 			MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType,
 			ServerHttpRequest request, ServerHttpResponse response) {
-		// TODO Auto-generated method stub
+		
+		HttpServletRequest req = ((ServletServerHttpRequest)request).getServletRequest();
+		HttpServletResponse res = ((ServletServerHttpResponse)response).getServletResponse();
+		
+		String refreshToken = body.getRefreshToken().getValue();
+		adicionarRefreshTokenNoCookie(refreshToken, req, res);
 		return null;
+	}
+
+	private void adicionarRefreshTokenNoCookie(String refreshToken, HttpServletRequest req, HttpServletResponse res) {
+		Cookie refreshTokenCookie = new Cookie("refreshToken", refreshToken);
+		refreshTokenCookie.setHttpOnly(true);
+		refreshTokenCookie.setSecure(false);//fazer em produção
+		
+		
 	}
 
 }
