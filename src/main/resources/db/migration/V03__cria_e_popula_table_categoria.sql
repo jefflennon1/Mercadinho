b1a0de2e82c0ea2bb3cdc51e13514002f96f53e4
jefferson lennon <jeffersonlennon1@outlook.com>
2020-08-22 14:12:44 -0300

    criando e populando tabela de catregoria

---
+++ src/main/resources/db/migration/V03__cria_e_popula_table_categoria.sql
@@ -0,0 +1,18 @@
+CREATE TABLE categoria(
+	id BIGINT(10) PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50) NOT NULL
+	
+)ENGINE=innoDB  DEFAULT CHARSET=utf8;
+
+INSERT INTO pessoa(nome) VALUES ("Laticínios");
+INSERT INTO pessoa(nome) VALUES ("Enlatados");
+INSERT INTO pessoa(nome) VALUES ("Limpeza");
+INSERT INTO pessoa(nome) VALUES ("Grãos");
+INSERT INTO pessoa(nome) VALUES ("Frios");
+INSERT INTO pessoa(nome) VALUES ("Fast Food");
+INSERT INTO pessoa(nome) VALUES ("Bebidas em Geral");
+INSERT INTO pessoa(nome) VALUES ("Bebidas Alcoólicas");
+INSERT INTO pessoa(nome) VALUES ("Higiene Pessoal");
+
+
+
